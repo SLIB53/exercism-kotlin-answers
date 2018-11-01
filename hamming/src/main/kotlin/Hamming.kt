0f2b01faecfa03cf59e6633c02ff7ab3fe0a11f4
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-11-01 01:20:07 -0500

    Refactor hamming

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,16 +1,9 @@
-val dnaMismatchExcept = IllegalArgumentException("left and right strands must be of equal length.")
-
 object Hamming {
     fun compute(dnaStrandA: String, dnaStrandB: String): Int {
-        if (dnaStrandA.length == dnaStrandB.length) {
-            val deltas = dnaStrandA
-                .toCharArray()
-                .zip(dnaStrandB.toCharArray()).map { (a, b) ->
-                    if (a == b) 0 else 1
-                }
-
-            return deltas.sum()
+        require(dnaStrandA.length == dnaStrandB.length) {
+            "left and right strands must be of equal length."
         }
-        else throw dnaMismatchExcept
+
+        return dnaStrandA.zip(dnaStrandB).count { it.first != it.second }
     }
 }
