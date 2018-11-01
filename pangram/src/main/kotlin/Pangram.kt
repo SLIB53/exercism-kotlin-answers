f930697093c2458160b7058f00960fece2704cbf
Akil Krishnan <akil.krishnan.SLIB53@gmail.com>
2018-11-01 01:33:21 -0500

    Rewrite pangram

--- pangram/src/main/kotlin/Pangram.kt
+++ pangram/src/main/kotlin/Pangram.kt
@@ -1,10 +1,6 @@
 object Pangram {
     fun isPangram(sentence: String): Boolean {
-        val letters = Regex("[a-z]").findAll(sentence.toLowerCase())
-            .toList()
-            .map({ m -> m.value })
-            .distinct()
-
-        return letters.count() == 26
+        return sentence.toLowerCase().toSet()
+            .containsAll(('a'..'z').toSet())
     }
 }
